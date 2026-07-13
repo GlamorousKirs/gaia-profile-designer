0b662383050e1cf6cc8ec6159bcfeb875818c274
Kirsten <kirstentashi@pm.me>
2026-07-13 16:11:19 +0800

    Fixed color opacity

--- app/components/colorpicker/colorpicker-colorpicker.tsx
+++ app/components/colorpicker/colorpicker-colorpicker.tsx
@@ -1,4 +1,8 @@
 import { HexAlphaColorPicker } from "react-colorful";
+import { colord, extend } from "colord";
+import namesPlugin from "colord/plugins/names";
+
+extend([namesPlugin]);
 
 interface ColorPickerUIProps {
 	color: string;
@@ -6,9 +10,16 @@ interface ColorPickerUIProps {
 }
 
 export const ColorPickerUI = ({ color, onChange }: ColorPickerUIProps) => {
+	const safeColor = colord(color).isValid() 
+		? colord(color).toHex() 
+		: "#000000";
+
 	return (
 		<div className="custom-layout">
-			<HexAlphaColorPicker color={color} onChange={onChange} />
+			<HexAlphaColorPicker 
+				color={safeColor} 
+				onChange={(newColor) => onChange(newColor)} 
+			/>
 		</div>
 	);
 };
