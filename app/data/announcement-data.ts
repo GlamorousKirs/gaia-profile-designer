c58a510be2e64ff1fb2234b5a556ab4a63e9f8fb
Kirsten <kirstentashi@pm.me>
2026-07-15 02:20:28 +0800

    .

--- app/data/announcement-data.ts
+++ app/data/announcement-data.ts
@@ -2,7 +2,7 @@ export const ANNOUNCEMENTS = [
 	{
 		date: "Update #2",
 		timestamp: "2026-07-15",
-		message: "Exporting and importing data has been fixed. You can now use it.",
+		message: "Exporting and importing data has been fixed; you can now use them.",
 		warning: "",
 		links: [
 			{
@@ -15,8 +15,8 @@ export const ANNOUNCEMENTS = [
 	{
 		date: "Update #1",
 		timestamp: "2026-07-14",
-		message: "There's some changes made. If you can't save data, please clear your browser storage.",
-		warning: "(Clicking the Clear Storage will immediately clear your site storage) Sorry for the inconvenience!",
+		message: "There have been some changes made, so most features that use storage will work properly. If you cannot save anything, please clear your browser storage.",
+		warning: "(Clicking the Clear Storage will immediately clear it) Sorry for the inconvenience!",
 		links: [
 			{
 				label: "Let me know",
