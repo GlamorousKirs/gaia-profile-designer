bdeb3dcc623f57627dbc27effa49dd15d5363f35
Kirsten <kirstentashi@pm.me>
2026-07-17 09:23:55 +0800

    .

--- app/routes/studio.tsx
+++ app/routes/studio.tsx
@@ -175,7 +175,7 @@ export default function Studio() {
 
 	return (
 		<TooltipProvider>
-			<div className="flex flex-col w-screen h-screen bg-background overflow-hidden select-none text-foreground relative bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+			<div className="flex flex-col w-screen h-screen bg-background overflow-hidden select-none text-foreground relative bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-size-[14px_24px]">
 				{!isMaximized && (
 					<StudioHeader
 						onOpenProfile={() => setIsProfileOpen(true)}
